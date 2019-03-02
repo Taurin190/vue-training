6b64762fcd3f2f4ead2aa7fe7f2c17646932d68d
Koichi Taura <taura.koichi@gmail.com>
2019-03-03 07:24:45 +0900

    5.event handling

--- intro_to_vue.js/main.js
+++ intro_to_vue.js/main.js
@@ -9,12 +9,23 @@ var app = new Vue({
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [{
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green-onWhite.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
